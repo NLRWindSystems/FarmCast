bc089907c1d43bbd7571b7c69961b4f87fc25d41
ptrbortolotti <ptrbortolotti@gmail.com>
2025-10-06 14:24:29 -0600

    update filename

--- test/test_examples.py
+++ test/test_examples.py
@@ -5,7 +5,7 @@ import subprocess
 EXAMPLES_DIR = os.path.join(os.path.dirname(__file__), "../examples")
 
 @pytest.mark.parametrize("example_script", [
-    "create_surrogate_torque2024.py",
+    "create_surrogate_torque2026.py",
 ])
 def test_example_scripts(example_script):
     """
